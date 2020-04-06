ff95f4bb970eaf50d58e7867b0a847666882b11e
Pavan S Sabnis <pvnsbns@gmail.com>
2020-04-06 16:04:32 -0700

    implement case sensitivity

--- app1.py
+++ app1.py
@@ -3,8 +3,12 @@ import json
 data = json.load(open('data.json'))
 
 def translate(w):
-    return data[w]
+    w = w.lower()
+    if w in data:
+        return data[w]
+    else:
+        return "The word doesn't exist. Please double-check it."
 
-word = input("Enter Word" )
+word = input("Enter Word: " )
 
 print(translate(word))
