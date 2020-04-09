5be091036f6e619384b74def5ed88578ed049712
Pavan S Sabnis <pvnsbns@gmail.com>
2020-04-09 00:06:06 -0700

    Fix case sensitivity issue

--- app1.py
+++ app1.py
@@ -4,11 +4,13 @@ from difflib import get_close_matches
 data = json.load(open('data.json'))
 
 def translate(w):
+    if w in data:
+        return data[w]
     w = w.lower()
     if w in data:
         return data[w]
     elif len(get_close_matches(w, data.keys())) > 0:
-        response = input('Did you mean {} instead? Y or N'.format(get_close_matches(w, data.keys())[0]))
+        response = input('Did you mean {} instead? Y or N \n'.format(get_close_matches(w, data.keys())[0]))
         if response == 'Y':
             return data[get_close_matches(w, data.keys())[0]]
         elif response == 'N':
