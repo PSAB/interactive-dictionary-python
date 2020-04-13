825743578aa5fe384ac523639a31adb9a303412f
Pavan S Sabnis <pvnsbns@gmail.com>
2020-04-13 09:21:08 -0700

    set up mysql remote DB connection

--- app1.py
+++ app1.py
@@ -4,13 +4,13 @@ from difflib import get_close_matches
 data = json.load(open('data.json'))
 
 def translate(w):
-    if w in data:
-        return data[w]
+    if w in data: # In case exact word w/ caps is found
+        return data[w] 
     w = w.lower()
-    if w in data:
-        return data[w]
-    elif len(get_close_matches(w, data.keys())) > 0:
-        response = input('Did you mean {} instead? Y or N \n'.format(get_close_matches(w, data.keys())[0]))
+    if w in data: # If all lowercase word is found
+        return data[w] 
+    elif len(get_close_matches(w, data.keys())) > 0: # Otherwise present close predictions
+        response = input('Did you mean {} instead? Y or N \n'.format(get_close_matches(w, data.keys())[0])) # Suggest the closest-matching word to user's word
         if response == 'Y':
             return data[get_close_matches(w, data.keys())[0]]
         elif response == 'N':
@@ -20,11 +20,13 @@ def translate(w):
     else:
         return "The word doesn't exist. Please double-check it."
 
-word = input("Enter Word: " )
 
-output = translate(word)
+
+word = input("Enter Word: " ) # Retrieve lookup word from user
+
+output = translate(word)  # display translate result from translate() function
 if type(output) == list:
     for item in output:
         print(item)
-else:
+else:               # If the word doesn't exist, just print the not-found string
     print(output)
